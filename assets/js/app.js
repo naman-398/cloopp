fbaef42a478e5f4b81bdb2830d4a50a9297cbdbe
naman-398 <you@example.com>
2024-03-05 10:32:47 +0530

    png to webp

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+    document.getElementById("preloder").classList.add("d-none");
+    document.body.classList.remove("overflow-hidden");
+}, 0);
+
+/////////back-to-top/////////
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("back-to-top");
+  
+    window.onscroll = function () {
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+            button.style.display = "block";
+        } else {
+            button.style.display = "none";
+        }
+    };
+  
+    button.onclick = function () {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    };
+  });
+
+function toggleNav() {
+    const body = document.body;
+    const navList = document.getElementById('nav-list');
+    const burgerMenu = document.getElementById('burger-menu');
+
+    body.classList.toggle('overflow_hidden');
+    navList.classList.toggle('show');
+
+
+    burgerMenu.classList.toggle('cross');
+}
